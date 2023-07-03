015f60fc58f595bba4368bff0d8a70c3fac19ff6
Ian Thompson <ian.thompson@hrblock.com>
2023-07-03 08:25:09 -0500

    modified:   is_question/api.py 	- reduced code to what was written in notebooks/Part01

--- is_question/api.py
+++ is_question/api.py
@@ -1,3 +1,4 @@
+"""contents of api.py"""
 from os import getenv
 from typing import Any
 
@@ -5,50 +6,35 @@ from stackapi import StackAPI
 
 
 class StackOverflow(StackAPI):
+    """A subclass of `StackAPI` that limits API calls to StackOverflow."""
+
     def __init__(self, key: str | None = None, **kwargs):
         if key is None:
-            key = self._get_key()
-        super().__init__(
-            name="stackoverflow", versions="2.2", key=key, **kwargs
-        )
+            key = getenv("stackapi_key")
+        super().__init__(name="stackoverflow", key=key, **kwargs)
         self._quota_remaining: int | None = None
 
-    @staticmethod
-    def _get_key() -> str | None:
-        return getenv(key="stackapi_key")
+    def fetch(
+            self, endpoint=None, page=1, key=None, filter='default', **kwargs
+    ) -> dict[str, Any]:
+        response = super().fetch(
+            endpoint=endpoint, page=page, key=key, filter=filter, **kwargs
+        )
+        self._quota_remaining = response.get("quota_remaining")
+        return response
 
     @property
     def quota_remaining(self) -> int | None:
         return self._quota_remaining
 
-    def fetch(
-        self,
-        endpoint: str | None = None,
-        page: int = 1,
-        key: str | None = None,
-        filter: str = "default",
-        **kwargs,
-    ) -> dict[str, Any]:
-        super().fetch.__doc__
-        results = super().fetch(
-            endpoint=endpoint,
-            page=page,
-            key=key,
-            filter=filter,
-            **kwargs,
-        )
-        self._quota_remaining = results.get("quota_remaining")
-        return results
-
     def fetch_questions(
-        self, question_ids: list[int] | None = None, **kwargs
+            self, question_ids: list[int] | None = None, **kwargs
     ) -> dict[str, Any]:
+        """Fetch questions from Stack Overflow."""
         endpoint = "questions"
         if question_ids is None:
-            questions = self.fetch(endpoint=endpoint, **kwargs)
+            response = self.fetch(endpoint=endpoint, **kwargs)
         else:
             endpoint += "/{ids}"
-            questions = self.fetch(
-                endpoint=endpoint, ids=question_ids, **kwargs
-            )
-        return questions
+            response = self.fetch(endpoint=endpoint, ids=question_ids, **kwargs)
+        return response
