b04af1bff8b82fc0d805007ee165922679d4e103
Ian Thompson <ian.thompson@hrblock.com>
2023-07-03 10:21:01 -0500

    modified:   is_question/utils.py 	- updated to reflect changes/notes in Part02 notebook

--- is_question/utils.py
+++ is_question/utils.py
@@ -1,9 +1,9 @@
+"""Contents of utils.py"""
 from w3lib import html
 
 
 def clean_text(text: str) -> str:
-    no_code = html.remove_tags_with_content(
-        text=text, which_ones=["code", "pre"]
-    )
+    """Remove code and HTML tags."""
+    no_code = html.remove_tags_with_content(text=text, which_ones=["code"])
     no_tags = html.remove_tags(text=no_code)
     return no_tags
